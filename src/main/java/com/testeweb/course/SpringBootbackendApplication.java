2f795f9ae756655add4a82fbcf75e676972a1da8
Danielscript1 <danielsousagit@gmail.com>
2021-12-27 17:28:42 -0300

    Criando Operacao de Instanciacao de Teste

--- src/main/java/com/testeweb/course/SpringBootbackendApplication.java
+++ src/main/java/com/testeweb/course/SpringBootbackendApplication.java
@@ -1,13 +1,30 @@
 package com.testeweb.course;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootbackendApplication {
+import com.testeweb.course.domain.Categoria;
+import com.testeweb.course.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SpringBootbackendApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootbackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"informatica");
+		Categoria cat2 = new Categoria(null,"escritorio");
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
