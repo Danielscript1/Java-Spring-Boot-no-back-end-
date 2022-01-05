764d84d7de0cb5799424d415051cf5019c0e7b3b
Danielscript1 <danielsousagit@gmail.com>
2022-01-05 12:25:09 -0300

    Pensonalizando o retorno da Validação ->adicionado Comentarios explicando as funcoes

--- src/main/java/com/testeweb/course/resources/exception/FieldMessage.java
+++ src/main/java/com/testeweb/course/resources/exception/FieldMessage.java
@@ -5,8 +5,9 @@ import java.io.Serializable;
 public class FieldMessage implements Serializable{
 	
 	/**
-	 * 
+	 * minha classe que busca os filtros desejados para exibir
 	 */
+	
 	private static final long serialVersionUID = 1L;
 	private String Fieldnome;
 	private String message;
--- src/main/java/com/testeweb/course/resources/exception/ValidationError.java
+++ src/main/java/com/testeweb/course/resources/exception/ValidationError.java
@@ -11,17 +11,19 @@ public class ValidationError extends StandardError {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	//receber os paramentros de erro da classe FieldMessage
 	private List<FieldMessage> errors = new ArrayList<>();
 	
+	//constructor vai bsucar os paramentros herdados da StandardErros que foi minha classe pensonalizada que criei
 	public ValidationError(Integer status, String msg, Long timeStamp) {
 		super(status, msg, timeStamp);
 		
 	}
-
+	//retrona os erros para json
 	public List<FieldMessage> getErrors() {
 		return errors;
 	}
-
+	//adicionando na lista os erros recebidos no paramentro e depois istancio para classe FielMessage
 	public void addError(String fielName, String messagem ) {
 		errors.add(new FieldMessage(fielName,messagem));
 	}
