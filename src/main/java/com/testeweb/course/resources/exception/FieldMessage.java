d7200abd1d9a3147b7ca6c1e470ab5b1cbba04c0
Danielscript1 <danielsousagit@gmail.com>
2022-01-05 12:15:43 -0300

    Pensonalizando o retorno da Validação

---
+++ src/main/java/com/testeweb/course/resources/exception/FieldMessage.java
@@ -0,0 +1,51 @@
+package com.testeweb.course.resources.exception;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String Fieldnome;
+	private String message;
+	
+	
+	public FieldMessage() {
+		
+	}
+
+
+	public FieldMessage(String fieldnome, String message) {
+		
+		this.Fieldnome = fieldnome;
+		this.message = message;
+	}
+
+
+	public String getFieldnome() {
+		return Fieldnome;
+	}
+
+
+	public void setFieldnome(String fieldnome) {
+		Fieldnome = fieldnome;
+	}
+
+
+	public String getMessage() {
+		return message;
+	}
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+	
+	
+	
+	
+}
--- src/main/java/com/testeweb/course/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/testeweb/course/resources/exception/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -21,10 +22,26 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 	
-	//esse metodo recebe a excecao e a requisicao
+	//esse metodo recebe a excecao da integridade de chave violada
 		@ExceptionHandler(DataIntegrityViolationException.class)
 		public ResponseEntity<StandardError> dataIntegrity(DataIntegrityViolationException e, HttpServletRequest request){
 			StandardError err = new  StandardError(HttpStatus.BAD_REQUEST.value(),e.getMessage(), System.currentTimeMillis());
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 		}
+		
+		
+		//esse metodo faz a exception de validação
+		@ExceptionHandler(org.springframework.web.bind.MethodArgumentNotValidException.class)
+		public ResponseEntity<StandardError> MethodArgumentNotValidException(org.springframework.web.bind.MethodArgumentNotValidException e, HttpServletRequest request){
+			ValidationError err = new  ValidationError(HttpStatus.UNPROCESSABLE_ENTITY.value(),"Erro de Validação!", System.currentTimeMillis());
+			
+			//antes de retorna o erro , tenho que percorrer a lista de erros 
+			
+			for (FieldError x : e.getBindingResult().getFieldErrors()) {
+				err.addError(x.getField(), x.getDefaultMessage());
+			}	
+			
+			return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(err);
+		}
+		
 }
